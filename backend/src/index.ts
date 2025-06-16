fe73fe429e8aab4d7defe32e817bd7a291d16009
preeeetham <spreethamkumar5@gmail.com>
2025-06-16 22:44:14 +0530

    update a user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,40 @@ import { PrismaClient } from "../generated/prisma";
 
 const prisma = new PrismaClient();
 
-async function insertUser(username: string, password:string, firstName: string,lastName: string){
-     const res = await prisma.user.create({
-        data: {
-            username,
-            password,
+// async function insertUser(username: string, password:string, firstName: string,lastName: string){
+//      const res = await prisma.user.create({
+//         data: {
+//             username,
+//             password,
+//             firstName,
+//             lastName
+//         }
+//      })
+//      console.log(res);
+// }
+
+// insertUser("admin","123455","preetham","kumar");
+
+interface UpdateParams{
+    firstName: string;
+    lastName: string;
+}
+
+async function updateUser(username:string,{
+    firstName,
+    lastName
+}:UpdateParams){
+    const res = await prisma.user.update({
+        where: {username},
+        data:{
             firstName,
             lastName
         }
-     })
-     console.log(res);
+    });
+    console.log(res);
 }
 
-insertUser("admin","123455","preetham","kumar");
+updateUser("admin",{
+    firstName:"preeeetham",
+    lastName: "kumarrrrr"
+})
